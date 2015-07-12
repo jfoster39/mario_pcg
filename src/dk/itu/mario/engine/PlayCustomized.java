118699e7abda505a80cc202f5a12485fa5afa204
Ty Foster <jfoster39@gatech.edu>
2015-07-12 15:20:05 -0400

    initial commit, added project files

---
+++ src/dk/itu/mario/engine/PlayCustomized.java
@@ -0,0 +1,34 @@
+package dk.itu.mario.engine;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class PlayCustomized {
+
+	public static void main(String[] args)
+     {
+
+     			String txtFileToUse = "player.txt";
+
+     			if (args.length!=0){
+     				txtFileToUse = args[0]+".txt";
+     			}
+
+		    	JFrame frame = new JFrame("Mario Experience Showcase");
+		    	MarioComponent mario = new MarioComponent(640, 480,true, txtFileToUse);
+
+		    	frame.setContentPane(mario);
+		    	frame.setResizable(false);
+		        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		        frame.pack();
+
+		        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		        frame.setLocation((screenSize.width-frame.getWidth())/2, (screenSize.height-frame.getHeight())/2);
+
+		        frame.setVisible(true);
+
+		        mario.start();   
+	}	
+
+}
