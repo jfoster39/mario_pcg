8b50e0d8b1df3d6a07481603b9fc1b701ac7a8d2
Ty Foster <jfoster39@gatech.edu>
2015-07-14 19:10:42 -0400

    Added bash script for running game(s) with optional parameter for running ant to rebuild project

---
+++ run_game.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Optional, pass in --build option in order to recompile using ant and build.xml
+if [ "$1" == "--build" ]; then 
+    ant 
+fi
+
+java -cp bin dk.itu.mario.engine.Play
+java -cp bin dk.itu.mario.engine.PlayCustomized
